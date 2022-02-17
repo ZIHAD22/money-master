b79034db9d2acd88ac9b623d0db3313514637b9a
ZIHAD22 <mdzihad2244@gmail.com>
2022-02-17 16:49:28 +0600

    saving function  and header text color  completed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// income section
 const calculateBtn = document.getElementById("calculate-btn")
 const mainIncome = document.getElementById("total-income");
 const foodCost = document.getElementById("cost-food");
@@ -5,10 +6,16 @@ const homeCost = document.getElementById("home-cost");
 const clothesCost = document.getElementById("clothes-cost");
 const exBalance = document.getElementById("expenses-blanc");
 const withOutExBalances = document.getElementById("without-ex-blanc");
+// Saving section
+const savingBtn = document.getElementById("saving-btn")
+const percentOfSave = document.getElementById("saving-input")
+const savingTo = document.getElementById("saving-to")
+const remainingBalance = document.getElementById("remaining-balance")
 
-// main function
+
+// income function
 calculateBtn.addEventListener("click" , function(){
-   if(mainIncome.value){
+   if(mainIncome.value && homeCost.value && foodCost.value && clothesCost.value){
     let incomeValue = parseInt(mainIncome.value);
     let foodCostValue = parseInt(foodCost.value);
     let homeCostValue = parseInt(homeCost.value);
@@ -17,7 +24,24 @@ calculateBtn.addEventListener("click" , function(){
     let restBalance = incomeValue - expensesValue
     exBalance.innerText = expensesValue;
     withOutExBalances.innerText = restBalance;
+
+    // saving function
+    savingBtn.addEventListener("click" , function(){
+       if(percentOfSave.value) {
+        const perSavingValue = parseInt(percentOfSave.value);
+        const preRestBalance = restBalance
+        const savingAmount = (restBalance * 20) / 100
+        const lastBalance = preRestBalance - savingAmount
+        savingTo.innerText = savingAmount
+        remainingBalance.innerText = lastBalance
+       }else{
+           console.log("Please Enter a saving percent ")
+       }
+    })
+
+
    }else{
        console.log("Please provide income value");
    }
 })
+
