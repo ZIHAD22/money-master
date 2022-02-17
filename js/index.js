ddb8e8ec3a12b1ae75c3194995aeea6f701733ad
ZIHAD22 <mdzihad2244@gmail.com>
2022-02-17 17:02:41 +0600

    nothing

--- js/index.js
+++ js/index.js
@@ -1,47 +1,48 @@
 // income section
-const calculateBtn = document.getElementById("calculate-btn")
-const mainIncome = document.getElementById("total-income");
-const foodCost = document.getElementById("cost-food");
-const homeCost = document.getElementById("home-cost");
-const clothesCost = document.getElementById("clothes-cost");
-const exBalance = document.getElementById("expenses-blanc");
-const withOutExBalances = document.getElementById("without-ex-blanc");
+const calculateBtn = document.getElementById('calculate-btn')
+const mainIncome = document.getElementById('total-income')
+const foodCost = document.getElementById('cost-food')
+const homeCost = document.getElementById('home-cost')
+const clothesCost = document.getElementById('clothes-cost')
+const exBalance = document.getElementById('expenses-blanc')
+const withOutExBalances = document.getElementById('without-ex-blanc')
 // Saving section
-const savingBtn = document.getElementById("saving-btn")
-const percentOfSave = document.getElementById("saving-input")
-const savingTo = document.getElementById("saving-to")
-const remainingBalance = document.getElementById("remaining-balance")
-
+const savingBtn = document.getElementById('saving-btn')
+const percentOfSave = document.getElementById('saving-input')
+const savingTo = document.getElementById('saving-to')
+const remainingBalance = document.getElementById('remaining-balance')
 
 // income function
-calculateBtn.addEventListener("click" , function(){
-   if(mainIncome.value && homeCost.value && foodCost.value && clothesCost.value){
-    let incomeValue = parseInt(mainIncome.value);
-    let foodCostValue = parseInt(foodCost.value);
-    let homeCostValue = parseInt(homeCost.value);
-    let clothesCostValue = parseInt(clothesCost.value);
+calculateBtn.addEventListener('click', function () {
+  if (
+    mainIncome.value &&
+    homeCost.value &&
+    foodCost.value &&
+    clothesCost.value
+  ) {
+    let incomeValue = parseInt(mainIncome.value)
+    let foodCostValue = parseInt(foodCost.value)
+    let homeCostValue = parseInt(homeCost.value)
+    let clothesCostValue = parseInt(clothesCost.value)
     let expensesValue = foodCostValue + homeCostValue + clothesCostValue
     let restBalance = incomeValue - expensesValue
-    exBalance.innerText = expensesValue;
-    withOutExBalances.innerText = restBalance;
+    exBalance.innerText = expensesValue
+    withOutExBalances.innerText = restBalance
 
     // saving function
-    savingBtn.addEventListener("click" , function(){
-       if(percentOfSave.value) {
-        const perSavingValue = parseInt(percentOfSave.value);
+    savingBtn.addEventListener('click', function () {
+      if (percentOfSave.value) {
+        const perSavingValue = parseInt(percentOfSave.value)
         const preRestBalance = restBalance
         const savingAmount = (restBalance * 20) / 100
         const lastBalance = preRestBalance - savingAmount
         savingTo.innerText = savingAmount
         remainingBalance.innerText = lastBalance
-       }else{
-           console.log("Please Enter a saving percent ")
-       }
+      } else {
+        console.log('Please Enter a saving percent ')
+      }
     })
-
-
-   }else{
-       console.log("Please provide income value");
-   }
+  } else {
+    console.log('Please provide income value')
+  }
 })
-
