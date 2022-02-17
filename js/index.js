43a61b36ab3c8db505dd55607ece12df56750ae4
ZIHAD22 <mdzihad2244@gmail.com>
2022-02-17 19:19:02 +0600

    some validation done

--- js/index.js
+++ js/index.js
@@ -11,21 +11,19 @@ const savingBtn = document.getElementById('saving-btn')
 const percentOfSave = document.getElementById('saving-input')
 const savingTo = document.getElementById('saving-to')
 const remainingBalance = document.getElementById('remaining-balance')
+// validation
+const validatorElement = document.getElementById('validation')
 
 // income function
 calculateBtn.addEventListener('click', function () {
-  if (
-    mainIncome.value &&
-    homeCost.value &&
-    foodCost.value &&
-    clothesCost.value
-  ) {
-    let incomeValue = parseInt(mainIncome.value)
-    let foodCostValue = parseInt(foodCost.value)
-    let homeCostValue = parseInt(homeCost.value)
-    let clothesCostValue = parseInt(clothesCost.value)
+  let incomeValue = parseInt(mainIncome.value)
+  let foodCostValue = parseInt(foodCost.value)
+  let homeCostValue = parseInt(homeCost.value)
+  let clothesCostValue = parseInt(clothesCost.value)
+  if (validator(incomeValue, foodCostValue, homeCostValue, clothesCostValue)) {
     let expensesValue = foodCostValue + homeCostValue + clothesCostValue
     let restBalance = incomeValue - expensesValue
+    
     exBalance.innerText = expensesValue
     withOutExBalances.innerText = restBalance
 
@@ -42,7 +40,44 @@ calculateBtn.addEventListener('click', function () {
         console.log('Please Enter a saving percent ')
       }
     })
-  } else {
-    console.log('Please provide income value')
   }
 })
+
+// validation text function
+function validationText(text, nameOfClass) {
+  validatorElement.innerText = text
+  if (nameOfClass) {
+    validatorElement.classList.add(nameOfClass)
+  } else {
+    validatorElement.classList.add('show')
+  }
+  return false
+}
+
+// negative input validation
+function negInputValidate() {
+  for (const value of arguments) {
+    if (value < 0) {
+      validationText('Please provide positive number')
+    }
+  }
+}
+
+// validation function
+function validator(income, foodCostValue, rent, clothesCostValue) {
+  if (!income && !foodCostValue && !rent && !clothesCostValue) {
+    validationText(`Please Provide Your Details To Calculation`)
+  } else if (!income) {
+    validationText(`Please Provide your income on the income input `)
+  } else if (!foodCostValue) {
+    validationText(`Please Provide your Food Market Budget in Food Input`)
+  } else if (!rent) {
+    validationText(`Please Provide Your house rent on the rent value`)
+  } else if (!clothesCostValue) {
+    validationText(`Please Provide Your Market Budget In Clothe Input`)
+  } else {
+    validatorElement.classList.remove('show')
+    return true
+  }
+  negInputValidate(income, foodCostValue, rent, clothesCostValue)
+}
